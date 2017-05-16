b13fcd6576eb4c737a785e52e378bf1421d1665f
boogie666 <bbugarschi@gmail.com>
2017-05-16 16:56:51 +0300

    blah

--- src/App.js
+++ src/App.js
@@ -5,47 +5,61 @@ import {Store, wrapStore} from "./redux.js"
 
 
 
-function appLogic(state, action){
-  if(action.type === "INC"){
-    return {
-      counter : state.counter + 1
-    };
-  }else if(action.type === "DEC"){
-    return {
-      counter : state.counter - 1
-    };
-  }
-
+function reducer(state, action){
   return state;
 }
 
+
 function render(dispatch, getState){
+  var store = {
+    dispatch,
+    getState
+  };
   return function(action){
     ReactDOM.render(
-      <App state={getState()}/>,
+      <App store={store}/>,
       document.getElementById('root')
     );
     return dispatch(action);
   }
 }
 
-var store = new Store({ counter : 0 }, appLogic);
+var store = new Store({}, reducer);
 store = wrapStore(store, [render]);
 
-function Hello({name, counter}){
-  return (
-    <div>
-      <div>Hello {name} {counter}</div>
-      <button onClick={()=>store.dispatch({type : "INC"})}>Click me! (Inc)</button>
-      <button onClick={()=>store.dispatch({type : "DEC"})}>Click me! (Dec)</button>
-    </div>
-  );
-}
+const cardStyle = {
+  maxWidth : "300px"
+};
 
-function App({state}){
+const cardImage = {
+  width : "100%"
+};
+
+const paddingMarginZero = {
+  padding : "0",
+  margin : "0",
+  marginBottom : "10"
+};
+
+function App({store}){
   return (
     <div className="App">
-      <Hello name="Boogie" counter={state.counter}/>
+      <div className="w3-container" style={paddingMarginZero}>
+        <h1 className="w3-teal" style={paddingMarginZero}>Pizza Scrapper</h1>
+      </div>
+      <div className="w3-container">
+        <div className="w3-card-2" style={cardStyle}>
+          <img src="http://www.dopopoco.ro/images/produse/pizza_diavolo_small.png" style={cardImage}/>
+          <div className="w3-container">
+            <h4>
+              <b>Pizza Diavolo</b>
+            </h4>
+            <p>
+              Ingrediente: the fires of hell its self
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
